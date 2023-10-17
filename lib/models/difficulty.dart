e69aab7a04c3fa57a689dc85f6b8074c99eb3bee
SomnusVeritas <sites@skup.in>
2023-10-17 18:05:35 +0200

    Added a getName function to convert enum names

--- lib/models/difficulty.dart
+++ lib/models/difficulty.dart
@@ -1,8 +1,12 @@
 class DifficultyUtil {
-  static List<String> get difficulties {
-    return List<String>.generate(Difficulty.values.length,
-        (index) => Difficulty.values.elementAt(index).name);
+  /// Converts lowerCamelCase or UpperCamelCase enum-names to 'normal' Strings
+  static String getName(Difficulty difficulty) {
+    String name = difficulty.name
+        .replaceAllMapped(RegExp(r'[A-Z]'), (match) => ' ${match[0]}');
+    name = name[0].toUpperCase() + name.substring(1);
+    return name;
   }
 }
 
+// Only use camelCase or UpperCamelCase for names
 enum Difficulty { veryEasy, easy, intermediate, hard, veryHard }
