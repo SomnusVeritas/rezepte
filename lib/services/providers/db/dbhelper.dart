c0fd6283c352144a59b0b4dd4fe5a2c1d34afce9
SomnusVeritas <sites@skup.in>
2023-11-13 17:32:05 +0100

    added db functions to delete and add recipes

--- lib/services/providers/db/dbhelper.dart
+++ lib/services/providers/db/dbhelper.dart
@@ -1,10 +1,7 @@
 import 'dart:io';
-
-import 'package:flutter/foundation.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:isar/isar.dart';
 import '../../../models/recipe.dart';
-import '../../../example_data.dart' as e;
 
 class DbHelper {
   static late Directory _dir;
@@ -20,17 +17,17 @@ class DbHelper {
 
   static Future<void> init() async {
     _dir = await getApplicationDocumentsDirectory();
-
-    if (kDebugMode) {
-      _isar.write((isar) => _isar.recipes.putAll(e.exampleRecipes));
-    }
   }
 
   static List<Recipe> fetchRecipes() {
-    return _isar.recipes
-        .getAll([1, 2])
-        .where((element) => element != null)
-        .cast<Recipe>()
-        .toList();
+    return _isar.recipes.where().findAll().cast<Recipe>().toList();
+  }
+
+  static void putRecipe(Recipe recipe) {
+    _isar.write((isar) => isar.recipes.put(recipe));
+  }
+
+  static bool deleteRecipe(Recipe recipe) {
+    return _isar.write((isar) => isar.recipes.delete(recipe.id));
   }
 }
