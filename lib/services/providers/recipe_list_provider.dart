384744729bbd4056a27ea2043c7714fa3c836560
SomnusVeritas <sites@skup.in>
2023-11-13 17:34:57 +0100

    changed provider to work with database

--- lib/services/providers/recipe_list_provider.dart
+++ lib/services/providers/recipe_list_provider.dart
@@ -2,27 +2,26 @@ import 'package:flutter/foundation.dart';
 import 'package:rezepte/services/providers/db/dbhelper.dart';
 
 import '../../models/recipe.dart';
-import 'package:rezepte/example_data.dart' as e;
 
 class RecipeListProvider extends ChangeNotifier {
-  final List<Recipe> _recipes = kDebugMode ? e.exampleRecipes : [];
+  // final List<Recipe> _recipes = [];
 
-  set recipes(List<Recipe> recipes) {
-    _recipes.clear();
-    _recipes.addAll(recipes);
-    notifyListeners();
-  }
+  // set recipes(List<Recipe> recipes) {
+  //   _recipes.clear();
+  //   _recipes.addAll(recipes);
+  //   notifyListeners();
+  // }
 
   List<Recipe> get recipes => DbHelper.fetchRecipes(); // _recipes;
 
-  void clearRecipes({silent = false}) {
-    _recipes.clear();
+  // void clearRecipes({silent = false}) {
+  //   _recipes.clear();
 
-    if (!silent) notifyListeners();
-  }
+  //   if (!silent) notifyListeners();
+  // }
 
   void addRecipe(Recipe recipe, {silent = false}) {
-    _recipes.add(recipe);
+    DbHelper.putRecipe(recipe);
     if (!silent) notifyListeners();
   }
 }
